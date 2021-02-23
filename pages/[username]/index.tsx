5a1b8761fab32b801608478b8c0260216c3e485e
Sham <hisham@alphapico.com>
2021-02-23 13:16:16 +0800

    routing

---
+++ pages/[username]/index.tsx
@@ -0,0 +1,7 @@
+export default function UserProfilePage({}) {
+  return (
+    <main>
+      <h1>Profile</h1>
+    </main>
+  );
+}
---
+++ pages/admin/index.tsx
@@ -0,0 +1,7 @@
+export default function AdminPostsPage({}) {
+  return (
+    <main>
+      <h1>Post</h1>
+    </main>
+  );
+}
